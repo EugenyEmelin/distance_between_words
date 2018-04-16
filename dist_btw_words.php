09723010f6a6be9f657b3ff56b29893981d461b1
Eugeny Emelin <emelin.eug2@ya.ru>
2018-04-16 21:57:49 +0300

    bug has been fixed

--- dist_btw_words.php
+++ dist_btw_words.php
@@ -1,13 +1,13 @@
 <?php
 //1
-$text1 = "the 1 1 1 the simple one words 5 5 the two words 6 6 the three five 2 5 6 words the"; 
+$text1 = "the 1 1 1 the simple one words 5 5 the two words 6 6 the three five 5 6 words the"; 
 $word1 = 'words';
 $word2 = 'the';
 echo "<br>Example 1:<br>";
 echo dist_btw_words($text1, $word1, $word2);
 
 //2
-$text2 = "Today is very good a sunny day and tomorrow will be very good day too";
+$text2 = "day fdsf dsfsdf dd is very good a sunny and tomorrow will be very good too Today dfsf day fsd Today";
 $word3 = 'day';
 $word4 = 'Today';
 echo "<br><br>Example 2:<br>";
@@ -32,14 +32,13 @@ function dist_btw_words($text, $word1, $word2) {
 		$current_word = $words[$i];
 		if ($current_word === $word1) { #если нашли первое слово
 			$last_position_w1 = $i; #сохраним его индекс в переменной
-			$distance = $last_position_w1 - $last_position_w2 - 1; #количество слов между между текущими найденными индексами word1 и word2
-			$current_max_dist = $distance;
+			$distance = abs($last_position_w1 - $last_position_w2) - 1; #количество слов между между текущими найденными индексами word1 и word2
 			if ($last_position_w2 >= 0 && $distance < $min_dist) { #если к этому моменту найден индекс вторго слова и рассчитаная на данной итерации дистанция меньше самой маленькой на данный момент дистанции
 				$min_dist = $distance; #присвоим минимальной дистанции текущее найденное расстояние
 			} 
 		} elseif ($current_word === $word2) { #если нашли второе слово
 			$last_position_w2 = $i; #сохраним его индекс в переменной
-			$distance = $last_position_w2 - $last_position_w1 - 1; #количество слов между между текущими найденными индексами word2 и word1
+			$distance = abs($last_position_w2 - $last_position_w1) - 1; #количество слов между между текущими найденными индексами word2 и word1
 			if ($last_position_w1 >= 0 && $distance < $min_dist) { #если к этому моменту найден индекс первого слова и рассчитаная на данной итерации дистанция меньше самой маленькой на данный момент дистанции
 				$min_dist = $distance; #присвоим минимальной дистанции текущее найденное расстояние
 			} 
@@ -52,12 +51,12 @@ function dist_btw_words($text, $word1, $word2) {
 	$min_pos_w2 = $keys_w2[0];
 	$max_pos_w2 = $last_position_w2;
 
-	$max_pos_w1w2 = $max_pos_w1 - $min_pos_w2 - 1; #максимальное расстояние между первым индексом word2 и последним word1
-	$max_pos_w2w1 = $max_pos_w2 - $min_pos_w1 - 1; #максимальное расстояние между первым индексом word1 и последним word2
-	$max_dist = $max_pos_w1w2 > $max_pos_w2w1 ? $max_pos_w1w2 : $max_pos_w2w1; #большее из этих расстояний и является максимальным расстоянием между словами
+	$max_dist_w1w2 = abs($max_pos_w1 - $min_pos_w2) - 1; #максимальное расстояние между первым индексом word2 и последним word1
+	$max_dist_w2w1 = abs($max_pos_w2 - $min_pos_w1) - 1; #максимальное расстояние между первым индексом word1 и последним word2
+	$max_dist = $max_dist_w1w2 > $max_dist_w2w1 ? $max_dist_w1w2 : $max_dist_w2w1; #большее из этих расстояний и является максимальным расстоянием между словами
 
 	$test_end = (microtime(true) - $test_start)*100000;
-	$response = "Минимальное расстояние: $min_dist <br> Максимальное расстояние: $max_dist <br> $test_end";
+	$response = "Минимальное расстояние: $min_dist <br> Максимальное расстояние: $max_dist <br> $test_end <br>";
 	return $response;
 }	
 
